bb58569b4501e8baa467ff9d36bf8a8f385f086b
SIBEL <97451391+sibeldemirel@users.noreply.github.com>
2025-05-05 23:24:18 +0200

    :sparkles: add homePage (#21)

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+      <header className="w-full p-4 bg-white shadow-md">
+        <div className="text-xl font-semibold">MemoryLeaf</div>
+      </header>
+    );
+  }
+  
